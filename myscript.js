c4172ec06c9252ba04248167a9cd35e601a18f7c
Levan <levan.loladzew@gmail.com>
2022-07-31 12:55:56 +0400

    add range slider plus function

--- myscript.js
+++ myscript.js
@@ -1,10 +1,17 @@
 const board = document.querySelector('.board');
-const grid = {rows:16, cols:16};
-const total = grid.rows * grid.cols;
+let grid = {rows:16, cols:16};
+let total = grid.rows * grid.cols;
 let color = 'black';
+let squares = board.querySelectorAll('div');
+let value = document.getElementById('value');
 
-
-
+function onValueChange(event){
+    const  eValue = event.value;
+    resetBoard();
+    grid = {rows:eValue, cols:eValue};
+    total = eValue * eValue;
+    createGrid(total);
+}
 createGrid(total);
 
 function createGrid(tot){
@@ -31,4 +38,11 @@ function colorSquare() {
 
 function changeColor(choice) {
     color = choice
+}
+
+
+function resetBoard(){
+    const board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
 }
